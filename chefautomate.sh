5ea671d81d71e75bf4473744be162361d9ed2799
sudheermareddy <sudheerm@sysgain.com>
2019-02-15 01:28:11 +0530

    Update chefautomate.sh

--- chefautomate.sh
+++ chefautomate.sh
@@ -14,11 +14,11 @@ orguser=$6
 
 ##Chef-Automate Upgrade
 
-sudo chef-marketplace-ctl upgrade -y
+#sudo chef-marketplace-ctl upgrade -y
 
 sudo chef-server-ctl reconfigure
 
-chef-server-ctl restart
+sudo chef-server-ctl restart
 
 ##Creating user for Chef Web UI
 
@@ -26,10 +26,10 @@ sudo touch /var/opt/delivery/.telemetry.disabled
 
 sudo automate-ctl create-user default $1 --password $5
 
-sudo chef-server-ctl user-create $1 $2 $3 $4 $5 > /etc/opscode/$1.pem
+sudo chef-server-ctl user-create $1 $2 $3 $4 $5 --filename  /etc/opscode/chefuser.pem /etc/opscode/$1.pem
 
-sudo chef-server-ctl org-create $6 "New Org" -a $1 > /etc/opscode/$6-validator.pem
-
+sudo chef-server-ctl org-create $6 "New Org" -a $1 --filename /etc/opscode/$6-validator.pem
+sudo chef-server-ctl reconfigure
 sudo chef-server-ctl install chef-manage 
 sudo chef-server-ctl reconfigure 
 sudo chef-manage-ctl reconfigure --accept-license
