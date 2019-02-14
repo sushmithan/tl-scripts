4716fc91f25697931f51806228dadbce0b7a4f12
sudheermareddy <sudheerm@sysgain.com>
2019-02-14 15:39:06 +0530

    Create chefautomate.sh

---
+++ chefautomate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+adminUsername=$1
+
+Firstname=$2
+
+Lastname=$3
+
+mailid=$4
+
+adminpassword=$5
+
+orguser=$6
+
+##Chef-Automate Upgrade
+
+chef-marketplace-ctl upgrade -y
+
+sudo chef-server-ctl reconfigure
+
+chef-server-ctl restart
+
+##Creating user for Chef Web UI
+
+sudo touch /var/opt/delivery/.telemetry.disabled
+
+sudo automate-ctl create-user default $1 --password $5
+
+sudo chef-server-ctl user-create $1 $2 $3 $4 $5 > /etc/opscode/$1.pem
+
+sudo chef-server-ctl org-create $6 "New Org" -a $1 > /etc/opscode/$6-validator.pem
