3a5f9f91ea26956e241b89fe82ade29b5e50170a
sudheermareddy <sudheerm@sysgain.com>
2019-02-15 13:53:29 +0530

    Update chefautomate.sh

--- chefautomate.sh
+++ chefautomate.sh
@@ -15,9 +15,5 @@ sudo automate-ctl create-user default $1 --password $5
 sudo chef-server-ctl user-create $1 $2 $3 $4 $5 --filename /etc/opscode/$1.pem
 sudo chef-server-ctl org-create $6 "New Org" -a $1 --filename /etc/opscode/$6-validator.pem
 sudo chef-server-ctl reconfigure
-sudo chef-server-ctl install chef-manage 
-pip=$(curl ifconfig.co)
-sudo hostname $pip
-sudo chef-server-ctl reconfigure 
-sudo chef-manage-ctl reconfigure --accept-license
+
 
