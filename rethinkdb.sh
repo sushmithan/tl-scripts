06f62e35d1030c5037fce882140f567509f7cb00
sushmithan <sushmithan@sysgain.com>
2019-02-25 17:58:10 +0530

    Create rethinkdb.sh

---
+++ rethinkdb.sh
@@ -0,0 +1,11 @@
+source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install rethinkdb -y
+sudo apt-get install build-essential protobuf-compiler python -y
+sudo apt-get install libprotobuf-dev libcurl4-openssl-dev -y
+sudo apt-get install libboost-all-dev libncurses5-dev -y
+sudo apt-get install libjemalloc-dev wget m4 -y
+sudo apt install python-pip -y
+pip install rethinkdb
+pip install --upgrade pip
